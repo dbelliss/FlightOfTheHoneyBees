483a308e6ffe054adc11d5ddafe3a85d8717dd13
dbelliss <daveybelliss@yahoo.com>
2018-01-27 16:32:33 -0800

    Erratic bee movement

--- Flight of the Honey Bees/Assets/Scripts/Bee.cs
+++ Flight of the Honey Bees/Assets/Scripts/Bee.cs
@@ -15,14 +15,47 @@ public class Bee : MonoBehaviour {
 	int maxHP = 1; // Health of bee
 
 	[SerializeField]
-	static float idleRange = 1f; // How close before idling
+	static float beginIdleRange = 1f; // How close before idling
+	static float breakIdleRange = 2f;
+	static float idleMovementRange = .5f;
+	public bool isIdling {
+		get {
+			return _isIdling;
+		}
+	}
+
+	public Vector2 idlePosition {
+		get {
+			return _idlePosition;
+		}
+	}
+		
+	private bool _isIdling = false;
+	private Vector2 _idlePosition;
+
 
 	Rigidbody2D rb;
+	BoxCollider2D bc;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		bc = GetComponent<BoxCollider2D> ();
 		BeeManager.bees [beeNumber] = this.gameObject;
+		if (beeNumber != BeeManager.curBee) {
+			DeactivateBee ();
+		}
+	}
 
+
+
+	void DeactivateBee() {
+		bc.enabled = false;
+		Debug.Log ("Disabling bee " + beeNumber.ToString ());
+	}
+
+	void ActivateBee() {
+		bc.enabled = true;
+		
 	}
 
 	// Update is called once per frame
@@ -54,18 +87,55 @@ public class Bee : MonoBehaviour {
 	// Follow the main bee
 	void FollowPlayer() {
 		int toFollowNum = (1 + this.beeNumber) % BeeManager.numBees;
-		Debug.Log (toFollowNum);
+
 		GameObject toFollow = BeeManager.bees [toFollowNum];
 		Vector2 vectorDistance = toFollow.transform.position - gameObject.transform.position;
 		float floatDistance = vectorDistance.magnitude;
 		// If not in idle range, continue to get close to cur bee
-		if (floatDistance > idleRange) {
+		Bee followBee = toFollow.GetComponent<Bee> ();
+		Debug.Log (floatDistance);
+		// If idling, leader must be farther away before following
+		if (isIdling) {
+			// Only break from idle is distance is greater than idle break
+			if (floatDistance > breakIdleRange) {
+				// Move closer to cur bee
+				_isIdling = false; // No longer idling
+				rb.velocity = vectorDistance.normalized * speed;
+			}
+		}
+		// If not idling, get within idle distance
+		else if (floatDistance > beginIdleRange) {
 			// Move closer to cur bee
+			_isIdling = false; // No longer idling
 			rb.velocity = vectorDistance.normalized * speed;
 		}
-		// Else in range, just circle around
+		// Just idle
 		else {
-			rb.velocity = Vector2.zero;
+			float idleFactor = .5f;
+			if (!_isIdling) {
+				Debug.Log("Beginning to idle");
+				_isIdling = true;
+				rb.velocity = Vector2.down * speed; // Default to down
+				_idlePosition = transform.position; // Save position before idling
+			}
+
+			//Move slightly up or down based on pivot
+			Vector2 pivot = followBee.idlePosition - Vector2.right;
+
+
+
+			// If above pivot, change velocity
+			if (transform.position.y >= pivot.y + idleMovementRange) {
+				// Move down
+
+				rb.velocity = Vector2.down * speed * idleFactor;
+				Debug.Log ("Setting velocity to " + rb.velocity.ToString ());
+			}
+			// If far below pivot, change velocity
+			else if (transform.position.y < pivot.y - idleMovementRange){
+				rb.velocity = Vector2.up * speed * idleFactor;
+				Debug.Log ("Setting velocity to " + rb.velocity.ToString ());
+			}
 		}
 	}
 
--- Flight of the Honey Bees/Assets/Scripts/BeeManager.cs
+++ Flight of the Honey Bees/Assets/Scripts/BeeManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BeeManager : MonoBehaviour {
-	public static int curBee = 1;
+	public static int curBee = 0;
 	public static GameObject[] bees = new GameObject[3];
 	public static int numBees = 3;
 	void Start () {
